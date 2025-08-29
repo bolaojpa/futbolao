443bc1e7d333f5f291537b3926e73af5d44ff642
Bolão Jp <bolaojpa@gmail.com>
2025-08-29 01:19:34 +0000

    Ficou quase perfeito, diminua mais o espaco entre o titulo CAMPEAO GERAL

--- src/components/fame/champion-banner.tsx
+++ src/components/fame/champion-banner.tsx
@@ -90,7 +90,7 @@ export function ChampionBanner({
               className="w-[16%] h-auto aspect-square rounded-full border-[0.5cqw] border-yellow-400"
             />
           )}
-          <p className={cn("font-bold", showPhotos ? "text-[5.5cqw]" : "text-[7cqw]")}>{campeaoGeralNome}</p>
+          <p className={cn("font-bold", showPhotos ? "text-[5cqw]" : "text-[5cqw]")}>{campeaoGeralNome}</p>
         </div>
       </Section>
 
@@ -109,7 +109,7 @@ export function ChampionBanner({
               className="w-[16%] h-auto aspect-square rounded-full border-[0.5cqw] border-yellow-400"
             />
           )}
-          <p className={cn("font-bold", showPhotos ? "text-[5cqw]" : "text-[6.5cqw]")}>{palpiteiroNome}</p>
+          <p className={cn("font-bold", showPhotos ? "text-[4.5cqw]" : "text-[4.5cqw]")}>{palpiteiroNome}</p>
         </div>
       </Section>
     </div>
