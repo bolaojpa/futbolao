dc141a6bb758a71f3c76b7e2179e26ceaaf93573
Bolão Jp <bolaojpa@gmail.com>
2025-09-08 15:55:34 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'digitalhub.fifa.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
