e0aa36be198211c6229dfc3f2310640a8ece627e
Bolão Jp <bolaojpa@gmail.com>
2025-10-09 15:58:38 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -36,7 +36,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https://',
+        protocol: 'https',
         hostname: 'pt.wikipedia.org',
         port: '',
         pathname: '/**',
